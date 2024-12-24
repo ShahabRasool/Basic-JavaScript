8e62393c913494e8231078f5a29ec0e394ce4291
ShahabRasool <shahabrasool12@gmail.com>
2024-12-24 15:36:23 +0500

    Added new JavaScript file

---
+++ index.js
@@ -0,0 +1,34 @@
+// if we add a string to number what will the data type of it
+// data type will be string
+let num = ""+ 5;
+console.log(typeof num);
+// console.log( "shahab");
+
+// if we have not a vaild number or string it will give ""NaN""" which mean not a number
+console.log( "shahab" - "rasool");
+
+// if we have two true value it will give 2
+// by defult valur of True is 1
+console.log(true + true);
+
+// Expression
+// expression is a value, value can be a number, string, boolean, null, undefined,
+// object, array, function
+// expression can be a single value or a combination of values
+// The combination of operand and operator is called Expression
+
+// assignment
+let a ="shahab";
+// arthematic operator
+var x= 8;
+var y = 5;
+console.log(x+y);
+var x= 8;
+var y = 5;
+console.log(x-y);
+var x= 8;
+var y = 5;
+console.log(x*y);
+var x= 8;
+var y = 5;
+console.log(x/y);
